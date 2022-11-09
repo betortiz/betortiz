559e490c2912fb0fba4ebfd608d8f19d89b56505
betortizPSG <betortiz_PSG@outlook.com.br>
2022-11-09 00:28:46 -0400

    Auteração n app.js

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://google.com.br"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Alberto Medeiros
         </a>
       </header>
     </div>
